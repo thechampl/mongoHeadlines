760e52ba2089d95b227a7816deb8cec2f0f37386
Luke Champ <luke.champ@outlook.com>
2018-04-28 13:20:21 -0400

    working on Heroku

--- server.js
+++ server.js
@@ -38,16 +38,16 @@ var db = mongojs(databaseUrl, collections);
 db.on("error", function (error) {
     console.log("Database Error:", error);
 });
-
-// // / If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = "process.env.MONGODB_URI" || "mongodb://localhost/scraper";
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGOLAB_WHITE_URI || "mongodb://localhost/scrapper";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
-    useMongoClient: true
+  useMongoClient: true
 });
+
 
 
 
